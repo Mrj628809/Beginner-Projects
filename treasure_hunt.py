21398134a609e9958e1c870f51359ae6c4fccf89
Mrj628809 <64451299+Mrj628809@users.noreply.github.com>
2022-08-09 00:16:28 -0400

    Update treasure_hunt.py

--- treasure_hunt.py
+++ treasure_hunt.py
@@ -23,9 +23,6 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-
-#Write your code below this line 👇
 fire = str("It's a room full of fire. Game Over.")
 
 treasure = str("You found the treasure! You Win!")
@@ -53,7 +50,6 @@ if crossroads == crossroads1 and crossroads1 =="right":
 elif crossroads == crossroads1 and crossroads1 == "left":
   print(hole)
   exit()
-
 elif crossroads == "":
   exit()
 
