3242af03a661bdaaf88bd62a0f69fd0b0ef87077
Mrj628809 <64451299+Mrj628809@users.noreply.github.com>
2022-08-09 00:01:09 -0400

    Update Pizza_Shop.py

--- Pizza_Shop.py
+++ Pizza_Shop.py
@@ -1,11 +1,9 @@
-# 🚨 Don't change the code below 👇
+#PIZZA SHOP
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
 total_price = 0
 if size == "S":
   total_price =+ 15
