3000792835cbfb7133f35a1bb671959de09ac405
Mrj628809 <64451299+Mrj628809@users.noreply.github.com>
2022-08-09 00:08:03 -0400

    Update height_average.py

--- height_average.py
+++ height_average.py
@@ -3,20 +3,16 @@ student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 print(student_heights)
-# 🚨 Don't change the code above 👆
 
-
-#Write your code below this row 👇
-#Write your code below this row 👇
 total = 0
 #sum function
 count = 0
 #length function
 for items in student_heights:
   count += 1
-  #it will run the code and count each item entered     in the list
+  #it will run the code and count each item entered in the list
   total += items
-  #it will run the code and add each integer item       entered in the list
+  #it will run the code and add each integer item entered in the list
 average = total/count
 print(round(average))
 
